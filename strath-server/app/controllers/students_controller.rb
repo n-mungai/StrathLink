bc7706898afe3163cf7a190dd42da3ecef7c0e5d
shady-mungai <shadrack.njau@strathmore.edu>
2024-07-04 18:41:59 +0300

    Restructured the show method in the controller

--- strath-server/app/controllers/students_controller.rb
+++ strath-server/app/controllers/students_controller.rb
@@ -1,45 +1,34 @@
 class StudentsController < ApplicationController
     def index
-        @students = Student.all
+        if params[:course]
+            @@students = Student.where(course: params[:course])
+        elsif params[:year]
+            @@students = Student.where(year: params[:year])
+        elsif params[:interests]
+            @@students = Student.where(interests: params[:interests])
+        end
 
-
-        render json: @students
+        render json: @@students
     end
 
     def show
-        if params[:course]
-            @students = @students.where(course: params[:course])
-        end
+        @student = Student.find_by(email: params[:email])
 
-        # filter students using year
-        if params[:year]
-            @students = @students.where(year: params[:year])
-        end
-
-        # using availability
-        if params[:availability]
-            @students = @students.where(availability: params[:availability])
-        end
-
-        if params[:email]
-            @students = @students.where(email: params[:email])
-        end
-
-        render json: @students
+        render json: @student
     end
 
     def create
-        student = Student.new(student_params)
-        if student.save
+        student = Student.new(student_params).save
+        if student
             render json: { status: 'created', student: student}, status: :created
         else
-            render json: { status: 'error', errors: student.errors.full_messages}, status: :404
+            render json: { status: 'error', errors: student.errors.full_messages}, status: :unprocessable_entity
         end
 
         render json: @student
     end
 
     def student_params
-        params.require(:student).permit(:name, :email, :password, :password_confirmation, :course, :availabile, :year, :interests, )
+        params.permit(:name, :email, :password, :password_confirmation, :course, :availabile, :year, :interests, )
       end
 end
