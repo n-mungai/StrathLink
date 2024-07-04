1052a9ee0ed1931eaa24725a21a7bd858bfbb4f7
shady-mungai <shadrack.njau@strathmore.edu>
2024-07-04 18:42:23 +0300

    set an articles relationship within the model

--- strath-server/app/models/student.rb
+++ strath-server/app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ApplicationRecord
+      # relationships with other tables
       has_many :sent_messages, class_name: 'Message', foreign_key: 'sender_id'
       has_many :received_messages, class_name: 'Message', foreign_key: 'receiver_id'
+
+      has_many :articles
 end
