e96c2b560d6ff98a72e2b212a67c33a2ef07af43
shady-mungai <shadrack.njau@strathmore.edu>
2024-07-05 00:32:34 +0300

    added has_secure password to ensure passwords are hidden

--- strath-server/app/models/student.rb
+++ strath-server/app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ApplicationRecord
       # relationships with other tables
+      has_secure_password
       has_many :sent_messages, class_name: 'Message', foreign_key: 'sender_id'
       has_many :received_messages, class_name: 'Message', foreign_key: 'receiver_id'
 
