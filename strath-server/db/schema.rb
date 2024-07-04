489229877f15b69261e0b4feff5a1230bfea81d8
shady-mungai <shadrack.njau@strathmore.edu>
2024-07-04 18:42:50 +0300

    Added the articles model

--- strath-server/db/schema.rb
+++ strath-server/db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_03_170707) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_04_133545) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.string "topic"
+    t.integer "student_id", null: false
+    t.string "writer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["student_id"], name: "index_articles_on_student_id"
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.integer "sender_id", null: false
@@ -35,6 +46,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_03_170707) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "students"
   add_foreign_key "messages", "students", column: "receiver_id"
   add_foreign_key "messages", "students", column: "sender_id"
 end
